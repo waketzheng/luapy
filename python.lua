89ecf3335ba25fcf9e2ba12bf6d65b0b5cfca5ba
wj <zhengwenjie@esoaru.com>
2021-07-10 11:52:26 +0800

    Add more docs

--- python.lua
+++ python.lua
@@ -1,7 +1,17 @@
 local py = {}
 
+-- 将数组拼接成字符串
+-- Example：
+--   py=require('python')
+--   py.join({ 'a', 'b' }, '/') --> 'a/b'
+--   py.join({ 'a', 'b' }) --> 'ab'
 py.join = table.concat
 
+-- 把字符串转换成整数
+-- Example:
+--   py.int('3') --> 3
+--   py.int('3.6') --> 3
+--   py.int('a', 16) --> 10
 function py.int(num, base)
     if type(num) == 'string' then
         if base then
@@ -14,27 +24,44 @@ function py.int(num, base)
     end
 end
 
+-- 判断字符串是否为纯数字
+-- Example: py.isdigit('123123') --> true
 function py.isdigit(str)
     return not str:match('%D') and str ~= ''
 end
 
+-- 判断字符串是否只有空格和换行符、制表符
+-- Example: py.is_blank('  \r\n')  --> true
 function py.is_blank(str)
     return not str:match('%S')
 end
 
+-- 去掉字符串左边的空格
+-- Example：py.lstrip(' \r\n abc \r\n') --> 'abc \r\n'
 function py.lstrip( s )
     local b = s:match('^%s*(.*)$')
     return b or ''
 end
+
+-- 去掉字符串右边的空格
+-- Example：py.lstrip(' \r\n abc \r\n') --> ' \r\n abc'
 function py.rstrip( s )
     local b = s:match('^(.-)%s*$')
     return b or ''
 end
+
+-- 去掉字符串两边的空格
+-- Example：py.lstrip(' \r\n abc \r\n') --> 'abc'
 function py.strip( s )
     local b = s:match('^%s*(.-)%s*$')
     return b or ''
 end
 
+-- 返回一个起点为start，步长为step，终点为stop-step的数组
+-- Example:
+--   py.range(5) --> {0, 1, 2, 3, 4}
+--   py.range(1, 5)  -->  {1, 2, 3, 4}
+--   py.range(1, 5, 2) --> {1, 3}
 function py.range(start, stop, step)
     -- return list of { start ... stop }
     local i = start
@@ -64,6 +91,13 @@ local function is_empty(tab)
     return next(tab) == nil
 end
 
+-- 判断table,string是否为空, int是否为零
+-- Example：
+--   py.bool({}) -> false
+--   py.bool(false) -> false
+--   py.bool(0) -> false
+--   py.bool('') -> false
+--   py.bool(' ') -> true
 function py.bool(any)
     if not any then
         return false
@@ -77,7 +111,7 @@ function py.bool(any)
     return any ~= 0
 end
 
--- convert table to be a JSON string
+-- 将table转换成内容可见的字符串（只展示前5个元素）
 -- tb = {a=1}  ==> repr(tb) == '{"a":1}'
 local function repr(tab,...)
     if type(tab) ~= 'table' then return tab end
